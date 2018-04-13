efc58e1a2127030508b64a95f7a5444f7451033a
Nikolayeva Yekaterina <38361065+yxiamera@users.noreply.github.com>
2018-04-14 00:55:45 +0300

    ADD_PP_P: добавлены комментарии на английском

--- libs/src/polynome/ADD_PP_P.c
+++ libs/src/polynome/ADD_PP_P.c
@@ -1,6 +1,8 @@
-//выполнила Николаева Екатерина
+//Об авторе:
+//Николаева Екатерина Валерьевна, ФКТИ гр.№7307
+//https://sites.google.com/site/nikolaevayekaterina7307/
+//e-mail: enterfox101@gmail.com
 #pragma once
-#include <stdio.h>
 #include <stdlib.h>
 #include "Polynome.c"
 #include "../rational/ADD_QQ_Q.c"
@@ -8,7 +10,7 @@
 
 
 mod_4 *ADD_PP_P(mod_4 P1, mod_4 P2) {
-    //определяем многочлены с большей и меньшей степенью
+    //finding polynomials with bigger and smaller amount of coefficients (needed to know which polynomial takes more memory)
     mod_4 *max_p, *min_p;
     if (P1.m>P2.m){
         max_p = &P1;
@@ -17,19 +19,24 @@ mod_4 *ADD_PP_P(mod_4 P1, mod_4 P2) {
         max_p = &P2;
         min_p = &P1;
     }
+    //allocate polynomial with max_p->m degree
     mod_4 *P = allocate_mod_4(max_p->m);
 
-    //складываем коэффициенты min_p с коэфф. многочлена max_p , т.к. степени их одночленов больше чем степень многочлена min_p
+    //adding coef. of min_p with coef. of max_p
     int *F1[2], *F2[2], **F;
     int i,j;
     for (i=0;i<=min_p->m;i++){
+        //for each coefficient create fractions for both polynomials
         F1[0] = max_p->numerator[i];
         F1[1] = max_p->denominator[i];
         F2[0] = min_p->numerator[i];
         F2[1] = min_p->denominator[i];
+        //add them together
         F = ADD_QQ_Q(F1,F2);
+        //set result coef. to result of the function
         P->numerator[i] = F[0];
         P->denominator[i] = F[1];
+        //free memory
         free(F1[0]);
         free(F1[1]);
         free(F2[0]);
@@ -37,13 +44,16 @@ mod_4 *ADD_PP_P(mod_4 P1, mod_4 P2) {
     }
 
 
-    //переносим оставшиеся коэффициенты из max_p
+    //copy the rest coef. of max_p into result
     for (i=min_p->m+1;i<=max_p->m;i++){
+        //allocate memory
         P->numerator[i] = (int *)malloc((max_p->numerator[i][0]+3) * sizeof(int));
         P->denominator[i] = (int *)malloc((max_p->denominator[i][0]+3) * sizeof(int));
+        //copy numerator
         for (j=0; j<=P->numerator[i][0]+2; j++) {
             P->numerator[i][j] = max_p->numerator[i][j];
         }
+        //copy denominator
         for (j=0; j<=P->denominator[i][0]+2; j++) {
             P->denominator[i][j] = max_p->denominator[i][j];
         }
@@ -52,16 +62,4 @@ mod_4 *ADD_PP_P(mod_4 P1, mod_4 P2) {
     return P;
 }
 
-//int main()
-//{
-//    mod_4 P1 = *init_mod_4();
-//    print_mod_4(P1);
-//    mod_4 P2 = *init_mod_4();
-//    print_mod_4(P2);
-//
-//    print_mod_4(*ADD_PP_P(P1,P2));
-//
-//    return 0;
-//}
-
-//время создания - около 7-ми часов
+//full completion time - about 7 hours
