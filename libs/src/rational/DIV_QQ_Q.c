da597b6877652772ce29bd6f8f73adeda986ff1e
Nikolayeva Yekaterina <38361065+yxiamera@users.noreply.github.com>
2018-04-13 21:34:58 +0300

    DIV_QQ_Q: переведены комментарии
    + убраны неиспользующиеся строки кода и добавлена инфа об авторе

--- libs/src/rational/DIV_QQ_Q.c
+++ libs/src/rational/DIV_QQ_Q.c
@@ -1,51 +1,32 @@
-﻿//выполнила Николаева Екатерина
+//Об авторе:
+//Николаева Екатерина Валерьевна, ФКТИ гр.№7307
+//https://sites.google.com/site/nikolaevayekaterina7307/
+//e-mail: enterfox101@gmail.com
 #pragma once
-#include <stdio.h>
 #include <stdlib.h>
 #include "../integer/MUL_ZZ_Z.c"
 
 int** DIV_QQ_Q (int** F1, int** F2) {
 
-    int** F = (int **)malloc(2 * sizeof(int*));
+    //allocate memory for resulting fraction
+    int** F = malloc(2 * sizeof(int*));;
 
-    //умножение на перевернутую дробь
-    F[0] = MUL_ZZ_Z(F1[0], F2[1]); //числитель
-    F[1] = MUL_ZZ_Z(F1[1], F2[0]); //знаменатель
+    //multiplication with "upside-down" fraction
+    F[0] = MUL_ZZ_Z(F1[0], F2[1]); //numerator
+    F[1] = MUL_ZZ_Z(F1[1], F2[0]); //denominator
 
-    //исправление знака знаменателя
-    if (F[1][1] == 1) {
-        F[1][1] = 0;
-        if (F[0][1] == 1) {
-            F[0][1] = 0;
-        } else {
-            F[0][1] = 1;
+    //fix sign for denominator
+    if (F[1][1] == 1) {         //if denominator is negative
+        F[1][1] = 0;            //set it to positive
+        //"multiply" numerator by -1
+        if (F[0][1] == 1) {     //if it's negative
+            F[0][1] = 0;        //set it to positive
+        } else {                //else (if it's positive)
+            F[0][1] = 1;        //set it to negative
         }
     }
 
     return F;
 }
-//
-//int main() {
-//    int **F1, **F2, **result;
-//    int den1, den2, num1, num2;
-//
-//    printf("Enter dividend (example 1/2): ");
-//    scanf("%d/%d", &num1, &den1);
-//    printf("Enter divisor (example 1/4): ");
-//    scanf("%d/%d", &num2, &den2);
-//
-//    F1 = malloc(2 * sizeof(int*));
-//    F2 = malloc(2 * sizeof(int*));
-//    F1[0] = init_integer(num1);
-//    F1[1] = init_integer(den1);
-//    F2[0] = init_integer(num2);
-//    F2[1] = init_integer(den2);
-//
-//    result = DIV_QQ_Q(F1, F2);
-//
-//    printf("Result: %d/%d\n",get_integer(result[0]),get_integer(result[1]));
-//
-//    return 0;
-//}
 
-//время выполнения - около 2-х часов
+//full completion time - about 2 hours
