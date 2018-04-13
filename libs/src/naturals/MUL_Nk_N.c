6964f8bd9628a0cebf5764c7ac4d0fa51fae09fc
hieveryone1 <38325909+hieveryone1@users.noreply.github.com>
2018-04-13 20:36:09 +0300

    Баги устранены
    Пофикшена запись числа и добавлен случай с 0

--- libs/src/naturals/MUL_Nk_N.c
+++ libs/src/naturals/MUL_Nk_N.c
@@ -1,20 +1,28 @@
 #pragma once
+#include "../../naturals.h"
 
-int *MUL_Nk_N(int *A, int k) {
-    int i;
+int *MUL_Nk_N(int *A, int k)
+{
+    int i, k1;
 
-    //основной алгоритм
-    int *B = (int *)malloc((A[0] + 2 + k) * sizeof(int));
+    if ((!A[0])&&(!A[2]))
+    //there's no point in adding more memory when 10^k*0 = 0
+        k1 = 0;
+    else k1 = k;
+    //allocate a new number with bigger size
+    int* B = malloc((A[0]+3+k1) * sizeof(int));
 
-    //копируем "служебные" значения
-    B[0] = A[0] + k;
+    //set size of this new number
+    B[0] = A[0]+k1;
+    //set sign
     B[1] = A[1];
-    //вставляем нули
-    for (i = 2; i < k + 2; i++)
+
+    //copy first A[0] digits from the old number
+    for (i = 2; i<=A[0]+2; i++)
+        B[i] = A[i];
+    //fill last k digits with zeros
+    for (i = A[0]+3; i<B[0]+3; i++)
         B[i] = 0;
-    //копируем цифры исходного числа
-    for (i = k + 2; i < B[0] + 2; i++)
-        B[i] = A[i - k];
 
     return B;
 }
