bdfc0eefd27a01fd363ca1bdf17738b2ced6f43e
iWasBurnInUSSR <sshedl22@gmail.com>
2018-04-13 23:45:27 +0300

    Исправлено приведение типов.Никита

--- libs/src/rational/MUL_QQ_Q.c
+++ libs/src/rational/MUL_QQ_Q.c
@@ -8,7 +8,7 @@
 //#include "MUL_QQ_Q.h"
 
 int **MUL_QQ_Q(int **fractionMultiplied, int **fractionMultiplier)
-{    int ** fraction = malloc(2 * sizeof(int*)); // it's a fraction { fraction[0] - numerator  fraction[1] - denominator}
+{    int ** fraction = (int**)malloc(2 * sizeof(int*)); // it's a fraction { fraction[0] - numerator  fraction[1] - denominator}
     if( NZER_N_B(fractionMultiplied[1]) == 1 ||  NZER_N_B(fractionMultiplier[1]) == 1) //  checking that denominator != 0
     {
         printf("Invalid input");
@@ -17,6 +17,6 @@ int **MUL_QQ_Q(int **fractionMultiplied, int **fractionMultiplier)
     //
     fraction[0] = MUL_ZZ_Z(fractionMultiplied[0],fractionMultiplier[0] ); // numerator of composition
     // MUL_ZZ_Z must malloc memory
-    fraction[1] = MUL_ZZ_Z(fractionMultiplied[1],fractionMultiplier[1]); // denominator of composition
+    fraction[1] = MUL_NN_N(fractionMultiplied[1],fractionMultiplier[1]); // denominator of composition
     return  fraction;
 }
