b3edb603f75be118174d6fb912a339c7172d34cb
Nikolayeva Yekaterina <38361065+yxiamera@users.noreply.github.com>
2018-04-14 00:27:26 +0300

    MUL_Nk_N: Преобразование функции для натуральных
    А было для целых

--- libs/src/naturals/MUL_Nk_N.c
+++ libs/src/naturals/MUL_Nk_N.c
@@ -9,23 +9,21 @@ int *MUL_Nk_N(int *A, int k)
 {
     int i, k1;
 
-    if ((!A[0])&&(!A[2]))
+    if ((!A[0])&&(!A[1]))
     //there's no point in adding more memory when 10^k*0 = 0
         k1 = 0;
     else k1 = k;
     //allocate a new number with bigger size
-    int* B = (int *)malloc((A[0]+3+k1) * sizeof(int));
+    int* B = (int *)malloc((A[0]+2+k1) * sizeof(int));
 
     //set size of this new number
     B[0] = A[0]+k1;
-    //set sign
-    B[1] = A[1];
 
     //copy first A[0] digits from the old number
-    for (i = 2; i<=A[0]+2; i++)
+    for (i = 1; i<=A[0]+1; i++)
         B[i] = A[i];
     //fill last k digits with zeros
-    for (i = A[0]+3; i<B[0]+3; i++)
+    for (i = A[0]+2; i<B[0]+2; i++)
         B[i] = 0;
 
     return B;
