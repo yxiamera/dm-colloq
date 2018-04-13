e990d61139467f0509e23d3169e4f18fb92d29e4
iWasBurnInUSSR <sshedl22@gmail.com>
2018-04-13 23:26:59 +0300

    Комменты переведены на английский, проблемы пофикшены.

--- libs/src/integer/ABS_Z_N.c
+++ libs/src/integer/ABS_Z_N.c
@@ -3,24 +3,22 @@
 #include <stdlib.h>
 #include "../../integer.h"
 
-//Абсолютная величина числа, результат - натуральное
-//Об авторе:
-//Петрова Елизавета Михайловна гр. № 7307, ФКТИ
+//Absolute value of a number, the result is a natural number
+//About the author:
+//Petrova Elizaveta Mikhailovna, group В№7307
 //https://sites.google.com/view/petrovaliza7307
 //e-mail: ostenok@gmail.com
 
 
-int* ABS_Z_N(int* num) {
+int* ABS_Z_N(int* num){
     int* result_num;
     int i;
-    int div = num[0]; //количество цифр в изначальном числе
+    int div = num[0];
 
     result_num = (int*)calloc(div + 1, sizeof(int));
-    result_num[0] = div;  //количество цифр в модуле числа совпадает с количеством цифр в самом числе
-    for (i = 1; i < div + 1; i++){  //копируем цифры исходного числа в искомое натуральное, начиная от старших разрядов к младшим
+    result_num[0] = div;
+    for (i = 1; i < div + 1; i++){  //copy all the digits of the initial number to the result number
         result_num[i] = num[i + 1];
     }
     return result_num;
 }
-//Время выполнения: 10 минут
-//Логических строк кода: 3 (???)
