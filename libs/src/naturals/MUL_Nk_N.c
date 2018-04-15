f0ea6e560226f210e9171b0d54512cc2cd01d44a
Nikolayeva Yekaterina <38361065+yxiamera@users.noreply.github.com>
2018-04-15 21:55:46 +0300

    MUL_Nk_N переделана под новый тип данных
    Клиент почему-то отправил старый реквест

--- libs/src/naturals/MUL_Nk_N.c
+++ libs/src/naturals/MUL_Nk_N.c
@@ -1,31 +1,33 @@
+/*Об авторе:
+Николаева Екатерина Валерьевна, ФКТИ гр.№7307
+https://sites.google.com/site/nikolaevayekaterina7307/
+e-mail: enterfox101@gmail.com*/
 #pragma once
-#include <stdlib.h>
-#include "../../naturals.h"
-//Об авторе:
-//Николаева Екатерина Валерьевна, ФКТИ гр.№7307
-//https://sites.google.com/site/nikolaevayekaterina7307/
-//e-mail: enterfox101@gmail.com
 
-int *MUL_Nk_N(int *A, int k)
-{
-    int i, k1;
+struct natural{
+	int n;
+	int number[400];
+};
 
-    if ((!A[0])&&(!A[1]))
-    //there's no point in adding more memory when 10^k*0 = 0
-        k1 = 0;
-    else k1 = k;
-    //allocate a new number with bigger size
-    int* B = (int *)malloc((A[0]+2+k1) * sizeof(int));
+struct natural MUL_Nk_N(struct natural A, int k) {
+    struct natural B;
+    int i;
 
-    //set size of this new number
-    B[0] = A[0]+k1;
-
-    //copy first A[0] digits from the old number
-    for (i = 1; i<=A[0]+1; i++)
-        B[i] = A[i];
-    //fill last k digits with zeros
-    for (i = A[0]+2; i<B[0]+2; i++)
-        B[i] = 0;
+    //if A is not 0
+    if ((A.n==1)&&(!A.number[0]))
+    {
+        //copy first A[0] digits from the old number with shifting
+        for (i = A.n-1; i >= 0; i--)
+            B.number[i+k] = A.number[i];
+        //set size of new number
+        B.n = A.n + k;
+        //filling first k digits of B with zeros
+        for (i = 0; i < k; i++)
+            B.number[i] = 0;
+    }
+    else B = A;
 
     return B;
 }
+
+//full completion time - 30 minutes
